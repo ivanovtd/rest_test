c15638a124e2eb027ca8fc8ffe89598d9552969c
ivanovtd <work@ivanovtd.ru>
2020-08-18 16:03:08 +0300

    second

---
+++ article/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Author(models.Model):
+    """Authoe model description"""
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    """Author as FK to author model"""
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    body = models.TextField()
+    author = models.ForeignKey(
+        'Author', related_name='articles', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
